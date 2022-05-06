efabd9fb6fa69b2ff1d456202fa71a432b8157d4
Ryan McQuerry <bebo925@gmail.com>
2022-05-06 15:40:12 -0400

    Update rich-text.blade.php

--- resources/views/components/input/rich-text.blade.php
+++ resources/views/components/input/rich-text.blade.php
@@ -4,7 +4,7 @@ $id = md5($attributes->wire('model'));
 <div class="rounded-md shadow-sm" x-data="{
         value:@entangle($attributes->wire('model')),
         isFocused(){ return document.activeElement !== this.$refs.trix },
-        setValue(){ this.$refs.trix.editor.loadHTML(this.value) },
+        setValue(){ this.$refs.trix.editor.loadHTML(this.value || '') },
     }" x-init="setValue(); $watch('value', () => isFocused() && setValue())" x-on:trix-change="value = $event.target.value" {{ $attributes->whereDoesntStartWith('wire:model') }}
     wire:ignore>
 
