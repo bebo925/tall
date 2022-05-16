1438308a7534e5ed79211e1a3b7f3d76cbef1c60
Ryan McQuerry <bebo925@gmail.com>
2022-05-16 13:58:40 -0400

    Create panel.blade.php

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,15 @@
+<div class="bg-white overflow-hidden shadow rounded-lg">
+    @isset($header)
+    <div class="border-b border-gray-200 px-4 py-5 sm:px-6">
+        {{$header}}
+    </div>
+    @endisset
+    <div class="px-4 py-5 sm:p-6">
+        {{$slot}}
+    </div>
+    @isset($header)
+    <div class="border-t border-gray-200 px-4 py-4 sm:px-6">
+        {{$footer}}
+    </div>
+    @endisset
+</div>
