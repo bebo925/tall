97acaa23d3efb4024731e0db34edc566ed703f47
Ryan McQuerry <bebo925@gmail.com>
2022-05-23 09:47:39 -0400

    Update index.blade.php

--- resources/views/components/panel/index.blade.php
+++ resources/views/components/panel/index.blade.php
@@ -1,9 +1,9 @@
 @props(['title' => null])
 <div class="bg-white overflow-hidden shadow rounded-lg">
     @if($title)
-    <div {{$attributes->class(['-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'])}} >
-        <div class="ml-4 mt-2">
-            <h3 class="text-lg leading-6 font-medium text-gray-900">{{$title}}</h3>
+    <div {{$attributes->class(['flex items-center justify-between flex-wrap sm:flex-nowrap px-3 sm:px-6 pt-3 sm:pt-6'])}} >
+        <div class="">
+            <h3 class="text-xl leading-6 font-medium text-gray-900">{{$title}}</h3>
         </div>
         <div class="ml-4 mt-2 flex-shrink-0">
             @isset($headerActions)
@@ -14,7 +14,7 @@
     @endif
 
     @if($body)
-    <div {{$body->attributes->class(['px-4 py-5 sm:p-6'])}}>
+    <div {{$body->attributes->class(['px-3 py-5 sm:p-10'])}}>
         {{$body}}
     </div>
     @endif
