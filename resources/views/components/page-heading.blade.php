23f9d36031e32b4ec2f3e50ee6ce11be05f4bc80
Ryan McQuerry <bebo925@gmail.com>
2022-05-31 10:26:30 -0400

    Update page-heading.blade.php

--- resources/views/components/page-heading.blade.php
+++ resources/views/components/page-heading.blade.php
@@ -3,6 +3,7 @@
     <div class="flex-1 min-w-0">
         <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
             {{$title}}
+            {{$slot}}
         </h2>
         @if(isset($meta))
         <div class="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
