152fe4c0b03ec6e6a6af33264ed532695b814b1e
Ryan McQuerry <bebo925@gmail.com>
2023-03-10 09:52:30 -0500

    Update modal.blade.php

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,7 +1,7 @@
 @props(['showClose' => true])
 <div {{$attributes->merge(['class' => 'relative'])}}>
     @if($showClose)
-    <button type="button" wire:click="$emit('closeModal')" class="absolute p-2 text-gray-500 rounded-full cursor-pointer ring-0 focus:ring-0 top-3 right-3 hover:bg-gray-200 hover:text-gray-700 focus:outline-none">
+    <button type="button" wire:click="$emit('closeModal')" class="absolute p-2 text-secondary-500 rounded-full cursor-pointer ring-0 focus:ring-0 top-3 right-3 hover:bg-secondary-200 hover:text-secondary-700 focus:outline-none">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
@@ -9,7 +9,7 @@
     @endif
 
     @if(!empty($title))
-    <div class="px-8 py-8 text-2xl text-gray-700 font-bold">
+    <div {{$title->attributes->class(['px-8 pt-8 pb-5 text-2xl text-secondary-700 font-bold'])}}>
         {{$title}}
     </div>
     @endif
@@ -19,7 +19,7 @@
     </div>
 
     @if(!empty($actions))
-    <div class="flex items-end justify-end px-8 py-4 space-x-2">
+    <div {{$actions->attributes->class(['flex items-end justify-end px-8 py-4 space-x-2'])}} >
         {{$actions}}
     </div>
     @endif
