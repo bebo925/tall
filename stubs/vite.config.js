bf879f3f82e789611ed5db9a9070e2d8c5cec6d5
Ryan McQuerry <bebo925@gmail.com>
2023-03-31 10:08:06 -0400

    Update vite.config.js

--- stubs/vite.config.js
+++ stubs/vite.config.js
@@ -1,50 +1,12 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import fs from "fs";
-import { homedir } from "os";
-import { resolve } from "path";
-
-let host = "";
 
 export default defineConfig({
     plugins: [
-        laravel(["resources/css/app.css", "resources/js/app.js"]),
-        {
-            name: "blade",
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith(".blade.php")) {
-                    server.ws.send({
-                        type: "full-reload",
-                        path: "*",
-                    });
-                }
-            },
-        },
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+            valetTls: "site.test",
+        }),
     ],
-    server: detectServerConfig(host),
 });
-
-function detectServerConfig(host) {
-    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`);
-    let certificatePath = resolve(
-        homedir(),
-        `.config/valet/Certificates/${host}.crt`
-    );
-
-    if (!fs.existsSync(keyPath)) {
-        return {};
-    }
-
-    if (!fs.existsSync(certificatePath)) {
-        return {};
-    }
-
-    return {
-        hmr: { host },
-        host,
-        https: {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certificatePath),
-        },
-    };
-}
